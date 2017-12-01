5b50df40bdd29343593e25cb08ee403b3548f4eb
Adam Prentice <adamjlprentice@gmail.com>
2017-12-01 14:54:50 +0000

    Day 1 Part 1

---
+++ AdventOfCode.Web/AdventOfCode.Web/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+
+namespace AdventOfCode.Web
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            StaticConfiguration.DisableErrorTraces = false;
+        }
+    }
+}
