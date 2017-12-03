92cab06f940de763566a469fe59c532e4ec7ab67
Adam Prentice <adamjlprentice@gmail.com>
2017-12-03 13:49:42 +0000

    Just a little refactor

---
+++ AdventOfCode.Web/AdventOfCode.Web/Requests/InputRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdventOfCode.Web.Requests
+{
+    public class InputRequest
+    {
+        public string Input { get; set; }
+    }
+}
---
+++ AdventOfCode.Web/AdventOfCode.Web/Responses/AnswerResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdventOfCode.Web.Responses
+{
+    public class AnswerResponse
+    {
+        public int Day { get; set; }
+
+        public int Answer1 { get; set; }
+
+        public int Answer2 { get; set; }
+    }
+}
